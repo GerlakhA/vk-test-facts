58b20d426d67755d05f25b0473e1549254de2a3f
Andrey Gerlakh <andrey@MacBook-Air-Andrey.local>
2024-03-11 13:28:16 +0300

    final

--- src/pages/get-info/model/types/schema.ts
+++ src/pages/get-info/model/types/schema.ts
@@ -1,8 +1,15 @@
 import * as Yup from 'yup'
 
+// export const schema = Yup.object().shape({
+// 	name: Yup.string()
+// 		.required('Введите имя')
+// 		.matches(/^[a-zA-Z]{3,}$/, 'Имя может содержать только буквы')
+// 		.min(3, 'Имя должно быть не менее 3 символов')
+// })
+
 export const schema = Yup.object().shape({
 	name: Yup.string()
 		.required('Введите имя')
-		.matches(/^[a-zA-Z]{3,}$/, 'Имя может содержать только буквы')
+		.matches(/^[a-zA-Z]$/, 'Имя может содержать только буквы')
 		.min(3, 'Имя должно быть не менее 3 символов')
 })
